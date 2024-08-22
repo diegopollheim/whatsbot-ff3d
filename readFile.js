402529517c110e790f98853c859371ac121ffda8
Diego Pollheim <diegopollheim@gmail.com>
2024-08-21 22:09:44 -0300

    org: Remoção arquivo de testes

--- readFile.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const path =require('path')
-const pathImages = path.resolve(__dirname, 'src', 'assets')
-
-fs.readdirSync(pathImages).forEach(file => {
-    console.log(pathImages, file);
-  });
